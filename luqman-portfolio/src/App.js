f740ab7bed930d03ba293148129fe5046f4c1d6f
Luqman Rumaiz <61153637+luqmanrumaiz@users.noreply.github.com>
2021-04-27 16:17:42 +0530

    Created Basic Page Skelton out of a Parallax Page Template

--- luqman-portfolio/src/App.js
+++ luqman-portfolio/src/App.js
@@ -3,19 +3,42 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+        <header><input id="navopen" type="checkbox" name="nav"/>
+            <div className="logo">
+                <h1 data-heading="L" className="name_title">uqman</h1>
+            </div>
 
-      </header>
-      <p>Scroll Up and Down this page to see the parallax scrolling effect.</p>
+        </header>
+        <main>
+            <article>
+                <div className="slider">
+                    <div className="slide slide-1" id="home">
+                        <div className="parallax"><img alt=""
+                                                       src="https://www.demix-sport.com/sportmaster.ru/sm-dev/img/item_photo2.png"/>
+                        </div>
+                        <h3 className="sub">Hey There I am Luqman</h3>
+                        <h1 className="title">Welcome to my Portfolio</h1><a className="btn" href="#">Let's start →</a>
+                    </div>
+                    <div className="slide slide-2" id="clothes">
 
-      <div className="parallax"></div>
+                    </div>
+                    <div className="slide slide-3" id="accessories">
 
-      <div style="height:1000px;background-color:red;font-size:36px">
-        Scroll Up and Down this page to see the parallax scrolling effect.
-        This div is just here to enable scrolling.
-        Tip: Try to remove the background-attachment property to remove the scrolling effect.
-      </div>
+                    </div>
+                    <div className="slide slide-4" id="shoes">
 
+                    </div>
+                </div>
+            </article>
+            <aside>
+                <ul className="nums">
+                    <li>scroll</li>
+                    <li>down</li>
+                </ul>
+            </aside>
+        </main>
+        <footer>
+        </footer>
     </div>
   );
 }
