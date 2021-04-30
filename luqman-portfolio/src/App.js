29a8f9d6605c68e0098696536fa1b37fa40a2ff8
Luqman Rumaiz <61153637+luqmanrumaiz@users.noreply.github.com>
2021-04-30 16:49:41 +0530

    Changed Page Designed Completely
    Now a Basic Parallax Design with a Sticky Header is made

--- luqman-portfolio/src/App.js
+++ luqman-portfolio/src/App.js
@@ -1,46 +1,77 @@
-import './App.css';
+import './App.scss';
 
 function App() {
-  return (
-    <div className="App">
-        <header><input id="navopen" type="checkbox" name="nav"/>
-            <div className="logo">
+
+    return (
+        <div className="App">
+            <div className="header" id="myHeader">
                 <h1 data-heading="L" className="name_title">uqman</h1>
             </div>
 
-        </header>
-        <main>
-            <article>
-                <div className="slider">
-                    <div className="slide slide-1" id="home">
-                        <div className="parallax"><img alt=""
-                                                       src="https://www.demix-sport.com/sportmaster.ru/sm-dev/img/item_photo2.png"/>
+            <div id="page">
+                <div id="layerSun">
+                    <img src="https://www.lukw4l.de/utils/media/assets/monochromaticLandscape/sun.svg"/>
+                </div>
+                <div id="layerClouds">
+                    <img src="https://www.lukw4l.de/utils/media/assets/monochromaticLandscape/clouds.svg"/>
+                </div>
+                <div id="container">
+                    <div id="parallax">
+                        <div className="layer" id="overlay">
                         </div>
-                        <h3 className="sub">Hey There I am Luqman</h3>
-                        <h1 className="title">Welcome to my Portfolio</h1><a className="btn" href="#">Let's start →</a>
+                        <div className="layer" id="layer0">
+                            <img src=""/>
+                        </div>
+                        <div className="layer" id="layer1"><img
+                            src="https://www.lukw4l.de/utils/media/assets/monochromaticLandscape/1.svg"/></div>
+                        <div className="layer" id="layer2"><img
+                            src="https://www.lukw4l.de/utils/media/assets/monochromaticLandscape/2.svg"/></div>
+                        <div className="layer" id="layer3">
+                            <img src="https://www.lukw4l.de/utils/media/assets/monochromaticLandscape/3.svg"/>
+                        </div>
+                        <div className="layer" id="layer4">
+                            <img src="https://www.lukw4l.de/utils/media/assets/monochromaticLandscape/4.svg"/>
+                        </div>
+                        <div className="layer" id="layer5">
+                            <img src="https://www.lukw4l.de/utils/media/assets/monochromaticLandscape/5.svg"/>
+                        </div>
+                        <div className="layer" id="layer6">
+                            <img src="https://www.lukw4l.de/utils/media/assets/monochromaticLandscape/6.svg"/>
+                        </div>
+                        <div className="layer" id="layerBalloon">
+                            <img src="https://www.lukw4l.de/utils/media/assets/monochromaticLandscape/balloon.svg"/>
+                        </div>
                     </div>
-                    <div className="slide slide-2" id="clothes">
-
-                    </div>
-                    <div className="slide slide-3" id="accessories">
-
-                    </div>
-                    <div className="slide slide-4" id="shoes">
-
+                    <div className="content">
                     </div>
                 </div>
-            </article>
-            <aside>
-                <ul className="nums">
-                    <li>scroll</li>
-                    <li>down</li>
-                </ul>
-            </aside>
-        </main>
-        <footer>
-        </footer>
-    </div>
-  );
+            </div>
+        </div>
+    );
 }
+
+function myFunction() {
+    const header = document.getElementById("myHeader");
+    const sticky = header.offsetTop;
+
+    if (window.pageYOffset > sticky) {
+        header.classList.add("sticky");
+    } else {
+        header.classList.remove("sticky");
+    }
+}
+
+window.onscroll = function() {
+    //console.log(document.getElementById("container").offsetHeight);
+    const height = document.getElementById("container").offsetHeight;
+    const scroll = document.getElementById("container").scrollTop
+    const opacity = 1 - ((height-scroll)/height);
+    myFunction();
+    console.log(opacity); document.getElementById("overlay").style.opacity = opacity;
+}
+
+
+
+
 
 export default App;
