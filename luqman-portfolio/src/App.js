6757d1927b019ba49b26a28b4d7c61607f10e5bd
Luqman Rumaiz <61153637+luqmanrumaiz@users.noreply.github.com>
2021-05-01 20:39:12 +0530

    Changed Content Div Color and Added Blending for Nav Bar Elements

--- luqman-portfolio/src/App.js
+++ luqman-portfolio/src/App.js
@@ -1,36 +1,41 @@
 import './App.scss';
 
+const Contrast = require('contrast-js');
+let contrast = new Contrast();
+
 function App() {
 
     return (
         <div className="App">
-            <nav>
-                <div className="logo">
-                    <h1 data-heading="L" className="name_title">uqman</h1>
-                </div>
-                <div className="nav-links">
-                    <ul>
-                        <li><img src="../images/github.svg" alt=""/></li>
-                        <li><img src="../images/behance.svg" alt=""/></li>
-                        <li><img src="../images/twitter.svg" alt=""/></li>
-                    </ul>
-                </div>
-            </nav>
-
-            <div>
-                <div id="container">
-                    <div id="parallax">
-                      
-                        <div className="layer" id="layer1">
-                            <h1>Hey I am Luqman Rumaiz <span id="red">Welcome to My Portfolio</span></h1>
-                        </div>
-
-                        <div className="layer" id="layer6">
-                            <img src="../images/castle-bg.svg"/>
-                        </div>
-
+            <div className="contrast-bg">
+                <nav>
+                    <div className="logo">
+                        <h1 data-heading="L" className="contrast-el" id="name_title">uqman</h1>
                     </div>
-                    <div className="content">
+                    <div className="nav-links">
+                        <ul>
+                            <li><img src="../images/github.svg" alt=""/></li>
+                            <li><img src="../images/behance.svg" alt=""/></li>
+                            <li><img src="../images/twitter.svg" alt=""/></li>
+                        </ul>
+                    </div>
+                </nav>
+
+                <div>
+                    <div id="container">
+                        <div id="parallax">
+
+                            <div className="layer" id="layer1">
+                                <h1>Hey I am Luqman Rumaiz <span id="red">Welcome to My Portfolio</span></h1>
+                            </div>
+
+                            <div className="layer" id="layer6">
+                                <img src="../images/castle-bg.svg"/>
+                            </div>
+
+                        </div>
+                        . <div className="content">
+                    </div>
                     </div>
                 </div>
             </div>
@@ -38,16 +43,5 @@ function App() {
     );
 }
 
-window.onscroll = function() {
-    //console.log(document.getElementById("container").offsetHeight);
-    const height = document.getElementById("container").offsetHeight;
-    const scroll = document.getElementById("container").scrollTop
-    const opacity = 1 - ((height-scroll)/height);
-    console.log(opacity); document.getElementById("overlay").style.opacity = opacity;
-}
-
-
-
-
-
 export default App;
+
