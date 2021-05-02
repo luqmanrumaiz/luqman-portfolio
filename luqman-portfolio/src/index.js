c15ca509597e61b972a41dd698749bc4da34d1c5
Luqman Rumaiz <61153637+luqmanrumaiz@users.noreply.github.com>
2021-05-02 23:14:09 +0530

    Moved App Files to Index

--- luqman-portfolio/src/index.js
+++ luqman-portfolio/src/index.js
@@ -1,11 +1,62 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import './index.scss';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+class App extends React.Component {
+
+    render() {
+
+        return (
+            <div className="App">
+                <nav>
+                    <div className="logo">
+                        <h1 data-heading="L" id="name_title">uqman</h1>
+                    </div>
+                    <div className="nav-links">
+                        <ul>
+                            <li><img src="../images/github.svg" alt=""/></li>
+                            <li><img src="../images/behance.svg" alt=""/></li>
+                            <li><img src="../images/twitter.svg" alt=""/></li>
+                        </ul>
+                    </div>
+                </nav>
+                <div className="sidenav">
+                    <div id="innernav">
+                        <a onClick={() =>
+                            // Scroll to a certain element
+                            document.querySelector(".parallax").scrollIntoView({
+                                behavior: 'smooth'
+                            })}>_</a>
+                        <a onClick={() =>
+                            // Scroll to a certain element
+                            document.querySelector(".content").scrollIntoView({
+                                behavior: 'smooth'
+                            })}>_</a>
+                        <a href=".parallax">_</a>
+                        <a>_</a>
+                    </div>
+                </div>
+
+                <div id="container">
+
+                    <div className="parallax">
+
+                        <div className="layer" id="layer1">
+                            <h1>Hey I am Luqman Rumaiz <span id="red">Welcome to My Portfolio</span></h1>
+                        </div>
+
+                        <div className="layer" id="layer6">
+                            <img src="../images/castle-bg.svg"/>
+                        </div>
+                    </div>
+
+                    <div className="content">
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
