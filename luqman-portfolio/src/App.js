313741abf94a8b711192bbdb741f03af5597cff7
Luqman Rumaiz <61153637+luqmanrumaiz@users.noreply.github.com>
2021-05-02 17:29:00 +0530

    Added Functionality to Side Nav Bar
    Uses the JS Method scrollIntoView with Smooth Behavior to Override Overflow

--- luqman-portfolio/src/App.js
+++ luqman-portfolio/src/App.js
@@ -16,13 +16,16 @@ function App() {
                     </ul>
                 </div>
             </nav>
-
             <div className="sidenav">
                 <div id="innernav">
-                    <a href="#parallax">_</a>
-                    <a href="#services">_</a>
-                    <a href="#clients">_</a>
-                    <a href="#contact">_</a>
+                    <a onClick={() =>
+                        // Scroll to a certain element
+                        document.querySelector(".content").scrollIntoView({
+                            behavior: 'smooth'
+                        })}>_</a>
+                    <a>_</a>
+                    <a>_</a>
+                    <a>_</a>
                 </div>
             </div>
 
@@ -44,30 +47,5 @@ function App() {
         </div>
     );
 }
-
-$(document).ready(function(){
-    // Add smooth scrolling to all links
-    $("a").on('click', function(event) {
-
-        // Make sure this.hash has a value before overriding default behavior
-        if (this.hash !== "") {
-            // Prevent default anchor click behavior
-            event.preventDefault();
-
-            // Store hash
-            var hash = this.hash;
-
-            // Using jQuery's animate() method to add smooth page scroll
-            // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-            $('html, body').animate({
-                scrollTop: $(hash).offset().top
-            }, 800, function(){
-
-                // Add hash (#) to URL when done scrolling (default click behavior)
-                window.location.hash = hash;
-            });
-        } // End if
-    });
-});
 export default App;
 
